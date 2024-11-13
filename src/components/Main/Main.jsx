5388b3382cd641d52ad0b7244a6aad5c9ad77bbf
kimtechnos <franciskimani559@gmail.com>
2024-11-13 16:08:48 +0300

    implemented the cards interface for gemini UI

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -18,6 +18,32 @@ const Main = () => {
           }}
         />
       </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello , Dev.</span>
+          </p>
+          <p>How can i help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>suggest beautiful places to see in the upcoming Dev Fest</p>
+            <img src={assets.compass_icon} alt="compass-icon" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept:Urban planning</p>
+            <img src={assets.bulb_icon} alt="compass-icon" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our Ajira trip</p>
+            <img src={assets.message_icon} alt="compass-icon" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="compass-icon" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
